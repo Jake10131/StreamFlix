b5a0471375e0216a47bc53a56c38402f43de1475
Jake10131 <54140807+Jake10131@users.noreply.github.com>
2023-01-24 19:51:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,31 +11,42 @@ def main():
  |_____/ \__|_|  \___|\__,_|_| |_| |_|_|    |_|_/_/\_\
 
   """)
-  name = input("Enter the movie name: ").strip()
-  print(f"Searching for {name}")
-  sitelink = f"https://seapi.link/?type=search&query={name}"
-  url_results = requests.get(sitelink).json()
-  results = url_results["results"]
-  status = url_results["status"]
-  movieurl = []
-  moviename=[]
-  index = 1
-  if (status == 200):
-    for result in results:
-      if (result["size"] > 0):
-        print(f'({index}){result["title"]} --> {result["quality"]}\n')
-        index += 1
-        movieurl.append(result["url"])
-        moviename.append(result["title"])
-  if movieurl:
-    choice = int(input("Enter the index of the moive: ").strip())
-    try:
-      stream = movieurl[choice -1]
-      print(f'Playing: {moviename[choice -1]}')
-      webbrowser.open(stream)
-    except IndexError:
-      print("Incorrect Index entered")
-  else:
-    print(f"No results found for {name}")
+  def streamflix():
+    name = input("Enter the movie name: ").strip()
+    print(f"Searching for {name}")
+    sitelink = f"https://seapi.link/?type=search&query={name}"
+    url_results = requests.get(sitelink).json()
+    results = url_results["results"]
+    status = url_results["status"]
+    movieurl = []
+    moviename=[]
+    index = 1
+    if (status == 200):
+      for result in results:
+        if (result["size"] > 0):
+          print(f'({index}){result["title"]} --> {result["quality"]}\n')
+          index += 1
+          movieurl.append(result["url"])
+          moviename.append(result["title"])
+    def stream(url,name):
+      choice = int(input("Enter the index of the moive: ").strip())
+      try:
+        print(f'Playing: {name[choice -1]}')
+        webbrowser.open(url[choice -1])
+      except IndexError:
+        print("Incorrect Index entered")
+        stream(url,name)
+    if movieurl:
+      stream(movieurl,moviename)
+    else:
+      print(f"No results found for {name}")
+      streamflix()
+    repeat=int(input(r'Do you want to play another movie? (type 1 for "Yes" or 2 for "No": ').strip())
+    if repeat==1:
+      streamflix()
+    else:
+      exit()
+  streamflix()
+  
 if __name__ == "__main__":
   main()
